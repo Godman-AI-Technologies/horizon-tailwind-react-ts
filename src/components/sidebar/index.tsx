70e0964291820cef3c20c67f89313b76375d87e7
Denis Beresnev <shanthi.bunch@gmail.com>
2024-06-05 21:58:42 +0300

    fix: horizon free to godman ai

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -25,7 +25,7 @@ const Sidebar = (props: {
 
       <div className={`mx-[56px] mt-[50px] flex items-center`}>
         <div className="ml-1 mt-1 h-2.5 font-poppins text-[26px] font-bold uppercase text-navy-700 dark:text-white">
-          Horizon <span className="font-medium">FREE</span>
+          Godman <span className="font-medium">AI</span>
         </div>
       </div>
       <div className="mb-7 mt-[58px] h-px bg-gray-300 dark:bg-white/30" />
