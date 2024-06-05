ff496c9225d0249a4a6aba3a5e8ec4c4c5612b89
Denis Beresnev <shanthi.bunch@gmail.com>
2024-06-05 22:03:39 +0300

    refactor: hid unusual block from profile header

--- src/views/admin/profile/index.tsx
+++ src/views/admin/profile/index.tsx
@@ -2,8 +2,6 @@ import Banner from "./components/Banner";
 import General from "./components/General";
 import Notification from "./components/Notification";
 import Project from "./components/Project";
-import Storage from "./components/Storage";
-import Upload from "./components/Upload";
 
 const ProfileOverview = () => {
   return (
@@ -11,14 +9,6 @@ const ProfileOverview = () => {
       <div className="w-ful mt-3 flex h-fit flex-col gap-5 lg:grid lg:grid-cols-12">
         <div className="col-span-4 lg:!mb-0">
           <Banner />
-        </div>
-
-        <div className="col-span-3 lg:!mb-0">
-          <Storage />
-        </div>
-
-        <div className="z-0 col-span-5 lg:!mb-0">
-          <Upload />
         </div>
       </div>
       {/* all project & ... */}
