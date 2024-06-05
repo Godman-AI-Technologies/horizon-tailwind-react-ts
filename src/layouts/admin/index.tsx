a8104391fe8ef85def7beca7c72b332423cc124f
Denis Beresnev <shanthi.bunch@gmail.com>
2024-06-06 00:06:55 +0300

    fix: h-screen in layout

--- src/layouts/admin/index.tsx
+++ src/layouts/admin/index.tsx
@@ -57,7 +57,7 @@ export default function Admin(props: { [x: string]: any }) {
 
   document.documentElement.dir = "ltr";
   return (
-    <div className="flex h-full w-full">
+    <div className="flex  h-screen w-full ">
       <Sidebar open={open} onClose={() => setOpen(false)} />
       {/* Navbar & Main Content */}
       <div className="h-full w-full bg-lightPrimary dark:!bg-navy-900">
