29e653e9d431761d4ffbbd8d5c95c965d64c4f91
RuslanApkin <ruslan-apkin@yandex.ru>
2024-06-07 17:48:47 +0500

    feat: added auth handler

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,19 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import RtlLayout from "layouts/rtl";
 import AdminLayout from "layouts/admin";
 import AuthLayout from "layouts/auth";
+import AuthHandler from "utils/auth";
 const App = () => {
   return (
     <Routes>
       <Route path="auth/*" element={<AuthLayout />} />
-      <Route path="admin/*" element={<AdminLayout />} />
+      <Route
+        path="admin/*"
+        element={
+          <AuthHandler>
+            <AdminLayout />
+          </AuthHandler>
+        }
+      />
       <Route path="rtl/*" element={<RtlLayout />} />
       <Route path="/" element={<Navigate to="/admin" replace />} />
     </Routes>
