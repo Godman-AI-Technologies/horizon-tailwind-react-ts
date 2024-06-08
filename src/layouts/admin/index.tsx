63a16bffb4371295172bd95c29f007cf3ff0ede9
RuslanApkin <ruslan-apkin@yandex.ru>
2024-06-08 17:10:39 +0500

    fix: fixed layout issues

--- src/layouts/admin/index.tsx
+++ src/layouts/admin/index.tsx
@@ -51,10 +51,10 @@ export default function Admin(props: { [x: string]: any }) {
 
   document.documentElement.dir = "ltr";
   return (
-    <div className="flex  h-screen w-full ">
+    <div className="flex min-h-screen w-full">
       <Sidebar open={open} onClose={() => setOpen(false)} />
       {/* Navbar & Main Content */}
-      <div className="h-full w-full bg-lightPrimary dark:!bg-navy-900">
+      <div className="w-full bg-lightPrimary dark:!bg-navy-900">
         {/* Main Content */}
         <main
           className={`mx-[12px] h-full flex-none transition-all md:pr-2 xl:ml-[313px]`}
@@ -67,7 +67,7 @@ export default function Admin(props: { [x: string]: any }) {
               secondary={getActiveNavbar(routes)}
               {...rest}
             />
-            <div className="pt-5s mx-auto mb-auto h-full min-h-[84vh] p-2 md:pr-2">
+            <div className="pt-5s mx-auto mb-auto min-h-screen p-2 dark:text-gray-200 md:pr-2">
               <Routes>
                 {getRoutes(routes)}
 
