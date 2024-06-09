7a2eafb3e7d11ac31ebfd94876ab84eae5b71bc4
Denis Beresnev <shanthi.bunch@gmail.com>
2024-06-09 14:14:01 +0300

    feat: fetch agents, triggers, notes and knowledge

---
+++ src/views/admin/dashboard/Agents.tsx
@@ -0,0 +1,36 @@
+import Cookies from "js-cookie";
+import { useEffect } from "react";
+
+export const Agents = () => {
+  useEffect(() => {
+    const fetchAgents = async () => {
+      const token = Cookies.get("accessToken");
+      const profileId = Cookies.get("profileId");
+
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_USER_API}/profiles/${profileId}/agents`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error("Failed to get agents");
+        }
+
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error("Error on getting agents:", error);
+      }
+    };
+
+    fetchAgents();
+  });
+  return <div>agents</div>;
+};
--- src/views/admin/dashboard/index.tsx
+++ src/views/admin/dashboard/index.tsx
@@ -1,14 +1,18 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import ContentLayout from "shared/ContentLayout/ContentLayout";
+import { Agents } from "./Agents";
+import { Knowledge } from "./Knowledge";
+import { Notes } from "./Notes";
+import { Triggers } from "./Triggers";
 
 const Dashboard = () => {
   return (
     <ContentLayout>
       <Routes>
-        <Route path="agents" element={<div>agents</div>} />
-        <Route path="notes" element={<div>notes</div>} />
-        <Route path="knowledge" element={<div>knowledge</div>} />
-        <Route path="triggers" element={<div>triggers</div>} />
+        <Route path="agents" element={<Agents />} />
+        <Route path="notes" element={<Notes />} />
+        <Route path="knowledge" element={<Knowledge />} />
+        <Route path="triggers" element={<Triggers />} />
         <Route path="/*" element={<Navigate to="agents" replace />} />
       </Routes>
     </ContentLayout>
