df070fdf66e12cadd877be4ab92b5c322db7e2d6
Denis Beresnev <shanthi.bunch@gmail.com>
2024-06-13 22:50:04 +0300

    feat: updated switch layout logic

--- src/shared/SwitchLayout/SwitchLayout.tsx
+++ src/shared/SwitchLayout/SwitchLayout.tsx
@@ -6,57 +6,41 @@ interface ISide {
 }
 
 interface LayoutProps {
-  leftSide: ISide;
-  rightSide: ISide;
+  sides: ISide[];
 }
 
-const SwitchLayout: React.FC<LayoutProps> = ({ leftSide, rightSide }) => {
-  const [selectedColumn, setSelectedColumn] = useState<"left" | "right">(
-    "left"
-  );
+export const SwitchLayout: React.FC<LayoutProps> = ({ sides }) => {
+  const [selectedColumn, setSelectedColumn] = useState<number>(0);
 
   return (
     <div className="mt-3 flex flex-col md:flex-row">
       <div className="mb-4 flex w-full justify-center md:hidden">
         <div className="flex w-full overflow-hidden rounded-full bg-gray-200">
-          <button
-            className={`flex-1 px-4 py-2 ${
-              selectedColumn === "left"
-                ? "bg-blue-500 text-white"
-                : "text-black bg-gray-200"
-            }`}
-            onClick={() => setSelectedColumn("left")}
-          >
-            {leftSide.title}
-          </button>
-          <button
-            className={`flex-1 px-4 py-2 ${
-              selectedColumn === "right"
-                ? "bg-blue-500 text-white"
-                : "text-black bg-gray-200"
-            }`}
-            onClick={() => setSelectedColumn("right")}
-          >
-            {rightSide.title}
-          </button>
+          {sides.map((side, index) => (
+            <button
+              key={index}
+              className={`flex-1 px-4 py-2 ${
+                selectedColumn === index
+                  ? "bg-blue-500 text-white"
+                  : "text-black bg-gray-200"
+              }`}
+              onClick={() => setSelectedColumn(index)}
+            >
+              {side.title}
+            </button>
+          ))}
         </div>
       </div>
-      <div
-        className={`flex-1 ${
-          selectedColumn === "left" ? "block" : "hidden"
-        } md:block`}
-      >
-        {leftSide.component}
-      </div>
-      <div
-        className={`flex-1 ${
-          selectedColumn === "right" ? "block" : "hidden"
-        } md:block`}
-      >
-        {rightSide.component}
-      </div>
+      {sides.map((side, index) => (
+        <div
+          key={index}
+          className={`flex-1 ${
+            selectedColumn === index ? "block" : "hidden"
+          } md:block`}
+        >
+          {side.component}
+        </div>
+      ))}
     </div>
   );
 };
-
-export default SwitchLayout;
