323f340c189bc087346b52668c7b81af689fb39d
RuslanApkin <ruslan-apkin@yandex.ru>
2024-06-14 14:41:56 +0500

    fix: added dark theme to chat component

--- src/views/admin/default/index.tsx
+++ src/views/admin/default/index.tsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <ContentLayout>
       <div>Info</div>
-      <Chat className="max-w-2xl rounded-md bg-white shadow-[0_0_50px_40px_#FFF]" />
+      <Chat className="max-w-2xl rounded-md bg-white shadow-[0_0_50px_40px_#FFF] dark:bg-navy-700 dark:shadow-navy-700" />
     </ContentLayout>
   );
 };
